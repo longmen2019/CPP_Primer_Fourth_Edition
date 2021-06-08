537cc581e2bac1c085b6edd2f9fe99f2e4771d1a
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-06-08 13:12:09 -0400

    Update Exercises Section 1.4.2.cpp

--- Exercises Section 1.4.2.cpp
+++ Exercises Section 1.4.2.cpp
@@ -1,6 +1,6 @@
 
   /*Exercise 1.9*/
-  /*What does the following for loop do? What is the final value of sum/ */
+  /*What does the following for loop do? What is the final value of sum? */
   int sum = 0;
   for (int i = -100 ; i <=100 ; ++i)
   sum +=i;
