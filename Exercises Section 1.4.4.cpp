f58a3f54a6b659b257a8f63b59ee4cb506e6a48f
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-06-13 18:00:02 -0400

    Update Exercises Section 1.4.4.cpp

--- Exercises Section 1.4.4.cpp
+++ Exercises Section 1.4.4.cpp
@@ -46,5 +46,7 @@ int main ()
 }
 
 /*Exercise 1.20:
-Write a program to sum the numbers in a users-specified range, omitting the if test that sets the upper and lower bounds. Predict what happens if the input is the numbers 7 and 3, in that order. Now run the program giving it the numbers 7 and 3, and see if the results match your expectation. If not, restudy the discussion on the for and while loop until you understand what happened.
+Write a program to sum the numbers in a users-specified range, omitting the if test that sets the upper and lower bounds. Predict 
+what happens if the input is the numbers 7 and 3, in that order. Now run the program giving it the numbers 7 and 3, and see 
+if the results match your expectation. If not, restudy the discussion on the for and while loop until you understand what happened.
 */
