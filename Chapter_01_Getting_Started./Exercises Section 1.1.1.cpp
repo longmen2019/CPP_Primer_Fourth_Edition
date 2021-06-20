f1e40723c5b925fd15566d76480e4f1f7d6546a9
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-06-20 17:13:33 -0400

    Update Exercises Section 1.1.1.cpp

--- Chapter_01_Getting_Started./Exercises Section 1.1.1.cpp
+++ Chapter_01_Getting_Started./Exercises Section 1.1.1.cpp
@@ -10,7 +10,8 @@ int main(){
 
 /*
 Exercise 1.2:
-Change the program to return -1. A return value of -1 is often treated as an indicator that the program failed. However, systems vary as to how (or even whether) they report a failure from main. Recompile and rerun your program to see how your system treats a failure indicator from main.
+Change the program to return -1. A return value of -1 is often treated as an indicator that the program failed. However,
+systems vary as to how (or even whether) they report a failure from main. Recompile and rerun your program to see how your system treats a failure indicator from main.
 
 */
 
