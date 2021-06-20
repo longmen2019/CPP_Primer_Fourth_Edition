268eb09b421ca101313c0ac46d43ecbe6ed9e735
longmen2019 <51213632+longmen2019@users.noreply.github.com>
2021-06-20 17:13:13 -0400

    Add files via upload

---
+++ Chapter_01_Getting_Started./Exercises Section 1.1.1.cpp
@@ -0,0 +1,20 @@
+/*
+Exercise 1.1:
+Review the documentation for your compiler and determine what file naming convention it uses. Compile and run the main program from page 2
+*/
+
+int main(){
+
+  return 0;
+}
+
+/*
+Exercise 1.2:
+Change the program to return -1. A return value of -1 is often treated as an indicator that the program failed. However, systems vary as to how (or even whether) they report a failure from main. Recompile and rerun your program to see how your system treats a failure indicator from main.
+
+*/
+
+int main(){
+
+  return -1;
+}
